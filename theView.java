5fdc9c517d1af869384b7a31d8488905c1e26de5
tadhg03 <tadhg_mccarthy@dlsu.edu.ph>
2018-03-10 05:08:24 +0800

    added label

--- theView.java
+++ theView.java
@@ -36,7 +36,7 @@ import javax.swing.table.DefaultTableModel;
 public class theView {
 
     private ArrayList<Event> events = new ArrayList<>();
-    
+
     //self
     public theView self;
 
@@ -55,7 +55,7 @@ public class theView {
     public Container pane;
     public JScrollPane scrollCalendarTable;
     public JPanel calendarPanel;
-    
+
     //What we added
     public JPanel info;
     public JPanel dayTab; //not done yet, uncomment when done
@@ -63,24 +63,23 @@ public class theView {
     public JPanel addTab; //not done yet, uncomment when done
     public JLabel eventLabel, pName; //pName is just name
     public JTextField eventBox = new JTextField();
-    public JLabel cmbYearInput, cmbMonthInput, cmbDayInput;
+    public JLabel YearInputLabel, MonthInputLabel, DayInputLabel; // for adding
+    public JLabel dateLabel; //for info
     public JButton addBtnEvent = new JButton("Add");
     public JCheckBox isTask;
     public JLabel sTimeLabel; //start time label
     public JLabel eTimeLabel; //end time label
     public JComboBox startTime, endTime; //fixing positions
     public JToggleButton isDay, isAgenda;
-    
+
     public JLabel nameEvent; //name of event/to-do item
-    public JTextField eName = new JTextField(); //textfield for events
-    public JButton aEvent; //add the event
-    
+
     public JLabel isToDoList; //label for to-do list
-    
+
     public JTable dayTable; //main day table
     public DefaultTableModel dayModel = new DefaultTableModel();
     public JScrollPane dayScroll;
-    
+
     public JTable agendaTable;
     public DefaultTableModel agendaModel = new DefaultTableModel();
     public JScrollPane agendaScroll;
@@ -134,7 +133,7 @@ public class theView {
                     String val = modelCalendarTable.getValueAt(i, j).toString();
                     for (int k = 0; k < events.size(); k++) {
                         if (Integer.parseInt(val) == events.get(k).day && month + 1 == events.get(k).month && year == events.get(k).year) {
-                            modelCalendarTable.setValueAt(modelCalendarTable.getValueAt(i, j) + " \u26AC", i, j); //change to not edit the calendar
+                            modelCalendarTable.setValueAt(modelCalendarTable.getValueAt(i, j) + " " + events.get(k).name, i, j); //change to not edit the calendar
                         }
                     }
                 }
@@ -165,23 +164,23 @@ public class theView {
         cmbYear = new JComboBox();
 
         //what we created
-        cmbYearInput = new JLabel("Year:");
-        cmbMonthInput = new JLabel("Month:");
-        cmbDayInput = new JLabel("Day:");
+        YearInputLabel = new JLabel("Year:");
+        MonthInputLabel = new JLabel("Month:");
+        DayInputLabel = new JLabel("Day:");
+        dateLabel = new JLabel("Today"); //fix later if you want to
         isTask = new JCheckBox();
         isDay = new JToggleButton("Day");
         isAgenda = new JToggleButton("Agenda");
         startTime = new JComboBox();
         endTime = new JComboBox();
-        pName = new JLabel("Calendar Program AD was here");
+        pName = new JLabel("Productivity Tool");
         sTimeLabel = new JLabel("Start Time:");
         eTimeLabel = new JLabel("End Time:");
         nameEvent = new JLabel("Event Name:");
-        aEvent = new JButton("Add");
         isToDoList = new JLabel("To-Do Task:");
         dayTable = new JTable(dayModel);
         agendaTable = new JTable(agendaModel);
-        
+
         btnPrev = new JButton("<<");
         btnNext = new JButton(">>");
         modelCalendarTable = new DefaultTableModel() {
@@ -189,7 +188,7 @@ public class theView {
                 return false;
             }
         };
-        
+
         calendarTable = new JTable(modelCalendarTable);
         calendarTable.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
@@ -198,9 +197,10 @@ public class theView {
                 String[] parts = modelCalendarTable.getValueAt(row, col).toString().split(" ");
                 //System.out.println(col + ", " + row);
 
-                cmbYearInput.setText("Year: " + cmbYear.getSelectedItem());
-                cmbMonthInput.setText("Month: " + monthLabel.getText());
-                cmbDayInput.setText("Day: " + parts[0]);
+                YearInputLabel.setText("Year: " + cmbYear.getSelectedItem());
+                MonthInputLabel.setText("Month: " + monthLabel.getText());
+                DayInputLabel.setText("Day: " + parts[0]);
+                dateLabel.setText(monthLabel.getText() + " " + parts[0] + ", " + cmbYear.getSelectedItem());
 
                 int month; //added until
 
@@ -208,7 +208,7 @@ public class theView {
 
             }
         });
-        
+
         scrollCalendarTable = new JScrollPane(calendarTable);
         calendarPanel = new JPanel(null);
         //added
@@ -219,14 +219,14 @@ public class theView {
         dayScroll = new JScrollPane(dayTable);
         agendaScroll = new JScrollPane(agendaTable);
         agendaTab.setVisible(false); //initially only the day is shown
-        
+
         calendarPanel.setBorder(BorderFactory.createTitledBorder("Calendar"));
         //added
         info.setBorder(BorderFactory.createTitledBorder("Information"));
         dayTab.setBorder(BorderFactory.createTitledBorder("Day"));
         agendaTab.setBorder(BorderFactory.createTitledBorder("Agenda"));
         addTab.setBorder(BorderFactory.createTitledBorder("Add"));
-        
+
         btnPrev.addActionListener(new btnPrev_Action());
         btnNext.addActionListener(new btnNext_Action());
         addBtnEvent.addActionListener(new btnAddEvent_Action());
@@ -241,7 +241,7 @@ public class theView {
         pane.add(agendaTab);
         pane.add(addTab);
         pane.add(info);
-        
+
         //calendarPanel
         calendarPanel.add(monthLabel);
         calendarPanel.add(yearLabel);
@@ -254,65 +254,64 @@ public class theView {
         calendarPanel.add(startTime);
         calendarPanel.add(endTime);
         calendarPanel.add(nameEvent);
-        calendarPanel.add(eName);
-        calendarPanel.add(aEvent);
         calendarPanel.add(isToDoList);
-        calendarPanel.add(cmbYearInput);
-        calendarPanel.add(cmbMonthInput);
-        calendarPanel.add(cmbDayInput);
+        calendarPanel.add(YearInputLabel);
+        calendarPanel.add(MonthInputLabel);
+        calendarPanel.add(DayInputLabel);
         calendarPanel.add(addBtnEvent);
         calendarPanel.add(btnPrev);
         calendarPanel.add(btnNext);
         calendarPanel.add(scrollCalendarTable);
-        
+
         //info
         info.add(isDay);
         info.add(isAgenda);
         info.add(pName);
-        
+        info.add(dateLabel);
+
         //day
         dayTab.add(dayScroll);
-        
+
         //agenda
         agendaTab.add(agendaScroll);
-        
+
         //setBounds pane
         calendarPanel.setBounds(0, 100, 340, 580);
         info.setBounds(0, 0, 1060, 100);
         dayTab.setBounds(340, 100, 720, 580);
         agendaTab.setBounds(340, 100, 720, 580);
         addTab.setBounds(340, 100, 720, 580);
-        
+
         //setBounds(x, y, width, height) CP
         monthLabel.setBounds(320 - monthLabel.getPreferredSize().width / 2, 50, 200, 50);
         yearLabel.setBounds(20, 610, 160, 40);
         eventLabel.setBounds(20, 625, 170, 50);
-        eventBox.setBounds(90, 640, 150, 20);
+        eventBox.setBounds(10, 330, 150, 20);
         nameEvent.setBounds(10, 305, 90, 25);
-        eName.setBounds(10, 330, 150, 20);
         isToDoList.setBounds(10, 350, 90, 25);
         cmbYear.setBounds(460, 610, 160, 40);
         isTask.setBounds(85, 350, 90, 25);
         sTimeLabel.setBounds(180, 305, 90, 25);
         startTime.setBounds(250, 305, 80, 25);
         eTimeLabel.setBounds(188, 335, 90, 25);
-        aEvent.setBounds(250, 365, 80, 25);
         endTime.setBounds(250, 335, 80, 25);
-        cmbYearInput.setBounds(250, 390, 80, 20);
-        cmbMonthInput.setBounds(250, 410, 80, 20);
-        cmbDayInput.setBounds(250, 430, 150, 20);
-        addBtnEvent.setBounds(90, 660, 60, 45);
+        YearInputLabel.setBounds(250, 390, 80, 20);
+        MonthInputLabel.setBounds(250, 410, 80, 20);
+        DayInputLabel.setBounds(250, 430, 150, 20);
+        addBtnEvent.setBounds(250, 365, 80, 25);
         btnPrev.setBounds(9, 25, 50, 25);
         btnNext.setBounds(281, 25, 50, 25);
         scrollCalendarTable.setBounds(10, 50, 320, 250); //this is the calendar itself
         dayScroll.setBounds(10, 25, 700, 550);
         agendaScroll.setBounds(10, 25, 700, 550);
-        
+
         //setBounds info
         isDay.setBounds(850, 25, 100, 50);
         isAgenda.setBounds(950, 25, 100, 50);
         pName.setBounds(100, 25, 200, 50);
         pName.setFont(new Font("Calibri", Font.PLAIN, 20));
+        dateLabel.setBounds(400, 25, 200, 50);
+        dateLabel.setFont(new Font("Calibri", Font.PLAIN, 20));
 
         frmMain.setResizable(false);
         frmMain.setVisible(true);
@@ -330,7 +329,7 @@ public class theView {
         }
         dayModel.addColumn("Time");
         dayModel.addColumn("Events");
-        
+
         agendaModel.addColumn("Time");
         agendaModel.addColumn("Tasks");
 
@@ -346,11 +345,11 @@ public class theView {
         calendarTable.setRowHeight(41);
         modelCalendarTable.setColumnCount(7);
         modelCalendarTable.setRowCount(6);
-        
+
         dayTable.setRowHeight(41);
         dayModel.setColumnCount(2);
         dayModel.setRowCount(48);
-        
+
         agendaTable.setRowHeight(41);
         agendaModel.setColumnCount(2);
         agendaModel.setRowCount(50);
@@ -359,7 +358,7 @@ public class theView {
         for (int i = yearBound - 100; i <= yearBound + 100; i++) {
             cmbYear.addItem(String.valueOf(i));
         }
-        
+
         //adding times to start and end
         int hour = 0, minute = 0;
         String time;
@@ -372,7 +371,7 @@ public class theView {
             startTime.addItem(time);
             endTime.addItem(time);
             dayTable.setValueAt(time, i, 0);
-            
+
             minute += 30;
             if (minute / 30 == 2) {
                 minute = 0;
@@ -470,11 +469,45 @@ public class theView {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
 
+            System.out.println(YearInputLabel.getText());
+            System.out.println(MonthInputLabel.getText());
+            System.out.println(DayInputLabel.getText());
+            if (YearInputLabel.getText() != null && MonthInputLabel.getText() != null && DayInputLabel.getText() != null) {
+                System.out.println("in");
+                int col = calendarTable.getSelectedColumn();
+                int row = calendarTable.getSelectedRow();
+
+                modelCalendarTable.setValueAt(modelCalendarTable.getValueAt(row, col) + "\n\uFFEE", row, col);
+
+                int month; //added until
+
+                String[] MonthParts = MonthInputLabel.getText().split(" ");
+
+                month = MonthToInt(MonthParts[1]);
+
+                String[] parts = DayInputLabel.getText().split(" ");
+                String[] parts2 = YearInputLabel.getText().split(" ");
+
+                String date = month + "/" + parts[1] + "/" + parts2[1];
+
+                System.out.println(parts[1]);
+                System.out.println(parts2[1]);
+                System.out.println(month);
+
+                if (isTask.isSelected()) {
+                    events.add(new Event(date, eventBox.getText(), "Green", startTime.getSelectedItem().toString(), endTime.getSelectedItem().toString())); //add drop box first for start and end time
+                } else {
+                    events.add(new Event(date, eventBox.getText(), "Blue", startTime.getSelectedItem().toString(), endTime.getSelectedItem().toString())); //add drop box first for start and end time
+                }
+//                PSVDCParser psvDataParser = new PSVDCParser(self);
+//
+//                psvDataParser.writeData(events);
+
+            }
+        }
     }
-    
+
     class isDay_Action implements ActionListener {
 
         @Override
@@ -484,9 +517,9 @@ public class theView {
             addTab.setVisible(false);
             isAgenda.setSelected(false);
         }
-        
+
     }
-    
+
     class isAgenda_Action implements ActionListener {
 
         @Override
@@ -496,7 +529,7 @@ public class theView {
             addTab.setVisible(false);
             isDay.setSelected(false);
         }
-        
+
     }
 
     public void setEvents(ArrayList<Event> events) {
